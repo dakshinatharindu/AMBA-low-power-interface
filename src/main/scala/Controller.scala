d906c25e7ad9b937b4aea12ebaee98377dda93ed
dakshinatharindu <tharinduokd@gmail.com>
2023-02-17 13:59:27 +0530

    structured the Controller

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -1,11 +1,20 @@
 package amba
 
 import chisel3._
+import chisel3.util._
+import amba.Constants._
+import Chisel.OUTPUT
 
-class Controller extends Module{
-    val io = IO(new Bundle {
-        
-    })
+class Controller extends Module {
+  val io = IO(new Bundle {
+    val PACTIVE = Input(UInt(N.W))
+    val PSTATE = Output(UInt(M.W))
+    val PREQ = Output(Bool())
+    val PACCEPT = Input(Bool())
+    val PDENY = Input(Bool())
+    val PRESETn = Output(Bool())
+  })
+
+  val STATE = RegInit(p_RESET)
+  
 }
-
-
